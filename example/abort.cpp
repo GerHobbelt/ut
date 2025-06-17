3757933f5465c71bf1729deb1010daac1c41d4fc
Ger Hobbelt <ger@hobbelt.com>
2025-06-17 17:48:39 +0200

    - various fixes for compiler errors and warnings (MSVC) - place `NDEBUG` config define statement at the very top of the source file: NDEBUG + _DEBUG settings should NOT change halfway through a parse.

--- example/abort.cpp
+++ example/abort.cpp
@@ -5,11 +5,11 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <boost/ut.hpp>
 #undef NDEBUG
+#include <boost/ut.hpp>
 #include <cassert>
 
-int main() {
+int main(void) {
   using namespace boost::ut;
 
   "abort"_test = [] {
